45d35c796157093e977dca9b885c0b49d97a129b
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-09-16 14:03:16 -0600

    Organize the code base

--- server/index.js
+++ server/index.js
@@ -2,30 +2,33 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import { userRouter } from "./src/routes/users.js";
+import { UserModel } from "./src/models/User.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/auth", userRouter);
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("App is up and running");
 });
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
+//Establish data base connction
+const uri = process.env.MONGODBLocal_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening to port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on http://localhost:${process.env.PORT || 5555}`);
+});
---
+++ server/src/routes/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/User.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+  if (user) {
+    return res.json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "User registered successfully" });
+});
+
+router.post("/login");
+
+export { router as userRouter };
