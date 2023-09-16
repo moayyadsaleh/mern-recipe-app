c0d1a7c52ce1353fa6922a75be305a58e1e8d73b
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-09-16 15:01:47 -0600

    Finish the register and the log in functionality

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -7,17 +7,58 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const user = await UserModel.findOne({ username: username });
-  if (user) {
-    return res.json({ message: "User already exists" });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new UserModel({ username, password: hashedPassword });
-  await newUser.save();
 
-  res.json({ message: "User registered successfully" });
+  // Check if both username and password are provided
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+  }
+
+  try {
+    // Check if the user with the same username already exists
+    const existingUser = await UserModel.findOne({ username: username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user with the hashed password
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
+  try {
+    const user = await UserModel.findOne({ username: username });
+
+    if (!user) {
+      return res.status(400).json({ message: "User doesn't exist" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Username or Password is incorrect" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token, userID: user._id });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 export { router as userRouter };
